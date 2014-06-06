2dc314ba586671fc41056a9a1423298e12a47b0d
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-05 23:27:07 -0400

    country model

--- catalog/models.py
+++ catalog/models.py
@@ -20,3 +20,11 @@ class ItemFormat(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=2)
+
+    def __unicode__(self):
+        return self.name
