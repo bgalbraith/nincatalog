19025cdcbaf58a3f3c4ca049879f01926f7acf61
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-06 00:35:28 -0400

    a bunch more models

--- catalog/models.py
+++ catalog/models.py
@@ -28,3 +28,43 @@ class Country(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class MusicLabel(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Track(models.Model):
+    related_tracks = models.ManyToManyField('self')
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Item(models.Model):
+    RARITY_CHOICES = (
+        (0, 'S'), (1, 'A'), (2, 'B'), (3, 'C'), (4, 'D'), (5, 'E'), (6, 'N/A')
+    )
+    category = models.ForeignKey(Category)
+    artist = models.ForeignKey(Artist)
+    format = models.ForeignKey(ItemFormat)
+    country = models.ForeignKey(Country)
+    label = models.ForeignKey(MusicLabel, null=True)
+
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    notes = models.CharField(max_length=200)
+    catalog_number = models.CharField(max_length=200)
+    upc = models.CharField(max_length=200)
+    packaging = models.CharField(max_length=200)
+    release_date = models.DateField(null=True, blank=True)
+    added_date = models.DateField()
+    rarity = models.IntegerField(choices=RARITY_CHOICES)
+
+    def __unicode__(self):
+        return self.name
