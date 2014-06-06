1e58e94d4c2696c49296439752fb1053dcc7e20a
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-06 01:00:45 -0400

    root index view, template, and route

--- nincatalog/urls.py
+++ nincatalog/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nincatalog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('catalog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
