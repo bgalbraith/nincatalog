493d9e9ed0cf7989e59fa11bff1a50165a53fb37
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-07 00:29:33 -0400

    template, view, and route for item page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,7 @@ from catalog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<category>[a-z0-9-]+)/$', views.category, name='category'),
+    url(r'^(?P<category_tag>[a-z0-9-]+)/$', views.category, name='category'),
+    url(r'^(?P<category_tag>[a-z0-9-]+)/(?P<item_tag>[a-z0-9-]+)/$', views.item,
+        name='item'),
 )
--- catalog/views.py
+++ catalog/views.py
@@ -5,8 +5,14 @@ def index(request):
     return render(request, 'catalog/index.html')
 
 
-def category(request, category):
-
+def category(request, category_tag):
     return render(request, 'catalog/category.html', {
-        "category": category
+        "category": category_tag
+    })
+
+
+def item(request, category_tag, item_tag):
+    return render(request, 'catalog/item.html', {
+        "category": category_tag,
+        "item": item_tag
     })
