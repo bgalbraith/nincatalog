127f01d4350241cd006d3d521da2f3557669893d
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-15 13:51:09 -0400

    refactored tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -2,34 +2,38 @@ from django.test import TestCase
 
 
 class CatalogViewsTestCase(TestCase):
-    fixtures = ['catalog_testdata.yaml']
+    fixtures = ['catalog_test_data.yaml']
 
-    def test_index(self):
+    def test_index_view(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
 
-    def test_category(self):
+    def test_category_view_invalid_category(self):
         resp = self.client.get('/strobe-light/')
         self.assertEqual(resp.status_code, 404)
 
+    def test_category_view_valid_category(self):
         resp = self.client.get('/the-fragile/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('category' in resp.context)
 
         category = resp.context['category']
-        self.assertEqual(category.pk, 14)
+        self.assertEqual(category.pk, 1)
         self.assertEqual(category.name, 'The Fragile')
 
-    def test_item(self):
+    def test_item_view_invalid_category_invalid_item(self):
         resp = self.client.get('/strobe-light/2/')
         self.assertEqual(resp.status_code, 404)
 
+    def test_item_view_valid_category_invalid_item(self):
         resp = self.client.get('/the-fragile/2/')
         self.assertEqual(resp.status_code, 404)
 
+    def test_item_view_invalid_category_valid_item(self):
         resp = self.client.get('/strobe-light/1/')
         self.assertEqual(resp.status_code, 404)
 
+    def test_item_view_valid_category_valid_item(self):
         resp = self.client.get('/the-fragile/1/')
         self.assertTrue(resp.status_code, 200)
         self.assertTrue('item' in resp.context)
