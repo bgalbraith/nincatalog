fedf4b390544d3419ab19c97bc1f46fcae186b7c
Byron Galbraith <byron.galbraith@gmail.com>
2014-06-14 22:18:44 -0400

    initial catalog view test and fixture

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CatalogViewsTestCase(TestCase):
+    fixtures = ['catalog_testdata.yaml']
+
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_category(self):
+        resp = self.client.get('/strobe-light/')
+        self.assertEqual(resp.status_code, 404)
+
+        resp = self.client.get('/the-fragile/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('category' in resp.context)
+
+        category = resp.context['category']
+        self.assertEqual(category.pk, 1)
+        self.assertEqual(category.name, 'The Fragile')
