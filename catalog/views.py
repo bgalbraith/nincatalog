a123370498f0f1b5962d617e2a9f19304c6f9979
Byron Galbraith <byron.galbraith@gmail.com>
2014-07-13 02:24:56 -0400

    header group correctly selected by category

--- catalog/views.py
+++ catalog/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def category(request, category_tag):
     _category = get_object_or_404(Category, tag=category_tag)
-    _group = Category.objects.filter(halo__gt=0)
+    _group = Category.objects.filter(halo__isnull=_category.halo is None)
     return render(request, 'catalog/category.html', {
         "category": _category,
         "group": _group
