b53eaee8e01b70d099d667d07e145eab00ceddb7
Byron Galbraith <byron.galbraith@gmail.com>
2014-07-14 01:31:03 -0400

    enable static files being served from single location for prod environment

--- nincatalog/settings.py
+++ nincatalog/settings.py
@@ -91,6 +91,7 @@ DATE_FORMAT = 'Y-m-d'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
