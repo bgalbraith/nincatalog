ebf1342f990b9deffd3229ed7bceb44960117249
Byron Galbraith <byron.galbraith@gmail.com>
2014-07-16 18:07:44 -0400

    filter out categories with no items

--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,8 @@ from catalog.models import Artist, Category, Country, Era, Item, MediaFormat, \
 
 def index(request):
     _halo_group = Category.objects.filter(halo__isnull=False)
-    _other_group = Category.objects.filter(halo__isnull=True)
+    _other_group = Category.objects.filter(halo__isnull=True) \
+        .annotate(n_item=Count('item')).filter(n_item__gt=0)
     _report_group = Report.objects.all()
     return render(request, 'catalog/index.html', {
         "halo_group": _halo_group,
@@ -19,7 +20,8 @@ def index(request):
 
 def category(request, category_tag):
     _category = get_object_or_404(Category, tag=category_tag)
-    _group = Category.objects.filter(halo__isnull=_category.halo is None)
+    _group = Category.objects.filter(halo__isnull=_category.halo is None) \
+        .annotate(n_item=Count('item')).filter(n_item__gt=0)
     return render(request, 'catalog/category.html', {
         "category": _category,
         "group": _group
@@ -30,7 +32,8 @@ def item(request, category_tag, item_tag):
     _category = get_object_or_404(Category, tag=category_tag)
     _item = get_object_or_404(Item, pk=item_tag)
     if _category == _item.category:
-        _group = Category.objects.filter(halo__isnull=_category.halo is None)
+        _group = Category.objects.filter(halo__isnull=_category.halo is None) \
+            .annotate(n_item=Count('item')).filter(n_item__gt=0)
         return render(request, 'catalog/item.html', {
             "category": _category,
             "group": _group,
@@ -46,12 +49,14 @@ def report(request, report_tag):
     if model is None:
         raise Http404
 
-    _entries = model.objects.annotate(n_items=Count('item')).filter(n_items__gt=0)
+    _entries = model.objects.annotate(n_items=Count('item')) \
+        .filter(n_items__gt=0)
     col_length = len(_entries) / _report.n_columns
     if len(_entries) % _report.n_columns != 0:
         col_length += 1
     _entries = [_entries[i::col_length] for i in range(col_length)]
-    _group = Category.objects.filter(halo__isnull=True)
+    _group = Category.objects.filter(halo__isnull=True) \
+        .annotate(n_item=Count('item')).filter(n_item__gt=0)
     return render(request, 'catalog/report.html', {
         "report": _report,
         "entries": _entries,
