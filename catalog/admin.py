7ef536b280055fc75293b800e09a687b7063d421
Byron Galbraith <byron.galbraith@gmail.com>
2015-01-19 22:55:38 -0500

    enable models in admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Artist, Category, MediaPackage, MediaFormat, \
+    Country, MusicLabel, Era, ItemRarity, Item, ItemImage
+
+admin.site.register(Artist)
+admin.site.register(Category)
+admin.site.register(MediaPackage)
+admin.site.register(MediaFormat)
+admin.site.register(Country)
+admin.site.register(MusicLabel)
+admin.site.register(Era)
+admin.site.register(ItemRarity)
+admin.site.register(Item)
+admin.site.register(ItemImage)
--- nincatalog/settings.py
+++ nincatalog/settings.py
@@ -47,6 +47,17 @@ LOCAL_APPS = (
 
 INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages'
+)
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
