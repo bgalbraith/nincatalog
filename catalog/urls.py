ac6d87c909533225a7f0fd530d645718ed3225e2
bgalbraith <byron.galbraith@gmail.com>
2022-05-06 23:01:29 -0400

    re_path instead of removed url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,18 +1,19 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.views.generic import RedirectView
 
 from catalog import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^by-(?P<report_tag>[a-z0-9-]+)/$', views.report, name='report'),
-    url(r'^by-(?P<report_tag>[a-z0-9-]+)/(?P<entry_tag>[a-z0-9-]+)/$',
-        views.report_details, name='report_details'),
-    url(r'^(?P<category_tag>[a-z0-9-]+)/$', views.category, name='category'),
-    url(r'^(?P<category_tag>[a-z0-9-]+)/(?P<item_tag>[a-z0-9-]+)/$',
-        views.item, name='item'),
-    url(r'^default\.asp$', RedirectView.as_view(pattern_name='index')),
-    url(r'^item\.asp$', views.legacy_item, name='legacy_item'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^about/$', views.about, name='about'),
+    re_path(r'^by-(?P<report_tag>[a-z0-9-]+)/$', views.report, name='report'),
+    re_path(r'^by-(?P<report_tag>[a-z0-9-]+)/(?P<entry_tag>[a-z0-9-]+)/$',
+            views.report_details, name='report_details'),
+    re_path(r'^(?P<category_tag>[a-z0-9-]+)/$',
+            views.category, name='category'),
+    re_path(r'^(?P<category_tag>[a-z0-9-]+)/(?P<item_tag>[a-z0-9-]+)/$',
+            views.item, name='item'),
+    re_path(r'^default\.asp$', RedirectView.as_view(pattern_name='index')),
+    re_path(r'^item\.asp$', views.legacy_item, name='legacy_item'),
 ]
