4015f51fc0a09740c4d07283c81cba7d69381c0f
Byron Galbraith <byron.galbraith@gmail.com>
2025-02-23 22:51:29 -0500

    support custom ordering for product images (#55)

--- merch/views.py
+++ merch/views.py
@@ -10,22 +10,22 @@ def index(request):
 
 
 def category(request, category_tag):
-    category = get_object_or_404(Category.objects.all(), tag=category_tag)
+    category = get_object_or_404(Category, tag=category_tag)
     return render(request, "merch/category.html", {"category": category})
 
 
 def product(request, category_tag, product_tag):
-    category = get_object_or_404(Category.objects.all(), tag=category_tag)
+    category = get_object_or_404(Category, tag=category_tag)
     pk = product_tag.split("-")[-1]
     product = get_object_or_404(Product, pk=pk)
 
     if category not in product.deep_categories():
         raise Http404
-
-    images = ProductImage.objects.filter(pk__in=product.get_productimage_order())
+    
+    images = product.productimage_set.all()
     # force caching of zoom spec if needed
     for image in images:
-        url = image.zoom.url
+        _ = image.zoom.url
     return render(
         request,
         "merch/product.html",
